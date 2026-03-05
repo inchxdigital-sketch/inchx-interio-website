9a6927ac2ce97132d68f075d9e8492546ce9d0b4
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-03-05 20:56:46 +0530

    whatsapp number added

--- src/pages/Whatsapp.js
+++ src/pages/Whatsapp.js
@@ -3,7 +3,7 @@ import "./Whatsapp.css";
 import { FaWhatsapp } from "react-icons/fa";
 
 function Whatsapp() {
-  const phoneNumber = "917032383138";
+  const phoneNumber = "918688379789";
   const message = "Hello, I would like to know more about your Interior Products and Services.";
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
